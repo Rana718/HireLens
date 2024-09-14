a48f184f3c5bad48152f70e1b8cdeef0bb435960
Rana718 <ranadolui718@gmail.com>
2024-09-14 22:41:54 +0530

    fix the env issue

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'drizzle-kit';
 
-const url = 'postgresql://hiredb_owner:Ohs1ILwjUv7l@ep-young-lab-a1o4fjjk.ap-southeast-1.aws.neon.tech/hiredb?sslmode=require';
-
 export default defineConfig({
   dialect: 'postgresql',
   schema: 'utils/schema.ts',
   dbCredentials: {
-    url: url,
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL as string,
   },
 });
