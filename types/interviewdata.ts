bde9b564540eb5df98d49c3d3074236d434387ef
Rana718 <ranadolui718@gmail.com>
2024-09-15 18:01:02 +0530

    interviewdata types

---
+++ types/interviewdata.ts
@@ -0,0 +1,10 @@
+
+export interface InterviewData {
+    mockId: string;
+    jsonMockResp: string;
+    jobPosition: string;
+    jobDesc: string;
+    jobExperience: string;
+    createdBy: string;
+    createdAt: string;
+}
