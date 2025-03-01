6c790efb8c1d5cea0fb3a4cc155e03ad49075d7b
Rana718 <ranadolui718@gmail.com>
2025-03-01 20:52:19 +0530

    new struct init

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'drizzle-kit';
-
-export default defineConfig({
-  dialect: 'postgresql',
-  schema: 'utils/schema.ts',
-  dbCredentials: {
-    url: process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL as string,
-  },
-});
--- utils/db.ts
+++
@@ -1,8 +0,0 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
-import * as schema from './schema';
-
-
-const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL as string);
-
-export const db = drizzle(sql, {schema});
